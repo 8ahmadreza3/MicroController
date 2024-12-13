9f417e0b872564d932ce2ed039f242865f606ee4
8ahmadreza3 <8arhp3@gmail.com>
2024-12-13 17:37:53 +0330

    play and show

--- Entrance Door/main.c
+++ Entrance Door/main.c
@@ -148,3 +148,106 @@ byte x5[8] = {
   0b10000,
   0b00000
 };
+
+void setup() {
+  pinMode(pirPin, INPUT);
+  pinMode(A0, INPUT);
+  pinMode(BUZZER_PIN, OUTPUT);
+  servo1.attach(servo1Pin);
+  servo2.attach(servo2Pin);
+  lcd.begin(20, 4);
+  lcd.clear();
+  lcd.createChar(1, empty);
+  lcd.createChar(2, fill);
+  lcd.createChar(3, x0);
+  lcd.createChar(4, x1);
+  lcd.createChar(5, x2);
+  lcd.createChar(6, x3);
+  lcd.createChar(7, x4);
+  lcd.createChar(8, x5);
+  Serial.begin(115200);
+}
+
+void loop() {
+  if(isMovement()){
+    openDoor(analogRead(A0));
+    play();
+  }
+  delay(250);
+}
+
+boolean isMovement(){
+  return digitalRead(pirPin) && !doorIsOpen;
+}
+
+void openDoor(int limit){
+  doorIsOpen = true ;
+  int time = 3500 ;
+  int max = map(limit, 0, 1023, 0, 90);
+  lcd.clear();
+  lcd.print("WELLCOME");
+  lcd.setCursor(4, 1);
+  lcd.print("to my project!!!");
+  for(int pos=0 ; pos<=max ; pos++) {
+    servo1.write(90-pos);
+    servo2.write(90-pos);
+    delay(35);
+    time -= 35 ;
+  }
+  delay(time);
+  lcd.clear();
+}
+
+void play(){
+  int size = sizeof(durations) / sizeof(int);
+  for (int note=0, i=0 ; note<size ; note++) {
+    // to calculate the note duration, take one second divided by the note type.
+    int duration = 1500 / durations[note];
+    tone(BUZZER_PIN, melody[note], duration);
+    // to distinguish the notes, set a minimum time between them.
+    int pauseBetweenNotes = duration * 1; //1.25
+    if(note >= 5 && note < 45){
+      fillLCD(note-5, 2);
+    } else if(note >= size-40){
+      showLogo(i++);
+    }
+    delay(pauseBetweenNotes);
+    noTone(BUZZER_PIN);
+  }
+}
+
+void fillLCD(int i, int charID){
+  // fill lcd Step by step
+  lcd.setCursor((int)i/4, i%4);
+  lcd.write(charID);
+  lcd.setCursor((int)(79-i)/4, (79-i)%4);
+  lcd.write(charID);
+}
+
+void showLogo(int i){
+  // show logo Step by step
+  lcd.setCursor((int)i/4, i%4);
+  if(i == 33){
+    lcd.write(1);
+    i = 46 ;
+    lcd.setCursor((int)i/4, i%4);
+    lcd.write(8);
+  } else if(i == 34){
+    lcd.write(1);
+    i = 45 ;
+    lcd.setCursor((int)i/4, i%4);
+    lcd.write(7);
+  } else if(i == 37){
+    lcd.write(3);
+    i = 42 ;
+    lcd.setCursor((int)i/4, i%4);
+    lcd.write(6);
+  } else if(i == 38){
+    lcd.write(4);
+    i = 41 ;
+    lcd.setCursor((int)i/4, i%4);
+    lcd.write(5);
+  } else {
+    fillLCD(i, 1);
+  }
+}
