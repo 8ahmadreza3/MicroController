14a4931aaeacff860cad644e81db135886d1891d
8ahmadreza3 <8arhp3@gmail.com>
2025-01-01 12:13:21 +0330

    turn on

--- Smart Watch/main.c
+++ Smart Watch/main.c
@@ -62,3 +62,65 @@ void loop() {
   login();
   goMenu();
 }
+
+void turnOn() {
+  if (isOn)
+    return ;
+
+  for (int i = 0 ; i < 10 ; ++i) {
+    if (!digitalRead(2))
+      return ;
+
+    delay(300);
+  }
+  palyAnimation();
+  isOn = true ;
+  delay(1000);
+  showLogo();
+}
+
+void showLogo() {
+  display.clearDisplay();
+  delay(50);
+  display.drawBitmap(0, 0, logo, 128, 64, WHITE);
+  for (int i = 0 ; i < 10 ; i++) {
+    if (i % 2 == 0)
+      display.drawRect(i, i, 128 - 2 * i, 64 - 2 * i, WHITE);
+    else
+      display.drawRect(i, i, 128 - 2 * i, 64 - 2 * i, BLACK);
+
+    display.display();
+    delay(100);
+  }
+  delay(500);
+  for (int i = 0 ; i < 5 ; i++) {
+    display.clearDisplay();
+    display.drawBitmap(0, 0, logo, 128, 64, WHITE);
+    for (int j = 0 ; j < 8 - 2 * i ; j++) {
+      if (j % 2 == 0)
+        display.drawRect(j, j, 128 - 2 * j, 64 - 2 * j, WHITE);
+      else
+        display.drawRect(j, j, 128 - 2 * j, 64 - 2 * j, BLACK);
+    }
+    display.display();
+    delay(100);
+  }
+}
+
+void palyAnimation() {
+  for (int i = 1 ; i <= 40 ; i++) {
+    if (i % 8 == 1) {
+      display.clearDisplay();
+    }
+    int noteDuration = 1200 / noteDurations[i - 1];
+    tone(13, melody[i - 1], noteDuration);
+    int pauseBetweenNotes = noteDuration * 1.5;
+    delay(pauseBetweenNotes);
+    noTone(13);
+    if (i % 8 == 0) {
+      display.drawBitmap(0, 0, animation[i / 8], 128, 64, WHITE);
+      display.display();
+    }
+  }
+  display.display();
+}
